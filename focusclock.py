e510bbe63358767e4af6678d40b24d1aaa7923bb
Waynebu <95230751+Waynebu@users.noreply.github.com>
2023-09-30 12:43:27 +0800

    Create focusclock.py

---
+++ focusclock.py
@@ -0,0 +1,13 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+  
+    while seconds > 0:
+        minutes, seconds = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(minutes, seconds)
+        print(timer, end='\r')
+        time.sleep(1)
+        seconds -= 1
+      
+    print("Time's up!")
